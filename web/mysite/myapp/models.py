417bdad104693b5f90c2f0c811b2b33ab91d8c5d
Achitapol888 <achitapol.t@kkumail.com>
2024-10-13 22:05:51 +0700

    Renew database

--- web/mysite/myapp/models.py
+++ web/mysite/myapp/models.py
@@ -3,12 +3,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-    user_ID = models.AutoField(primary_key=True)  # Auto-incrementing unique ID
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=30, default='Anonymous')
     surname = models.CharField(max_length=30, default='Anonymous')
     phone_number = models.CharField(max_length=15, default='Anonymous')
-    student_ID = models.CharField(max_length=15, unique=True)
+    student_ID = models.CharField(max_length=15, unique=True, primary_key=True)
     kku_mail = models.EmailField(max_length=100, unique=True)
     dorm = models.CharField(max_length=30, default='Anonymous')
 
